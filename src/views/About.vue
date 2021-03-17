de6ea96006dda2c82963dc83235559132c324d3a
yifan_chen <yifan_chen@himirror.com>
2021-03-17 10:30:38 +0800

    init

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div class="about" id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+  let app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+</script>
