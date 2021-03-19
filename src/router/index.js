cdb38efd8573857948f8db4876c9d67344c0df50
yifan <yifan_chen@himirror.com>
2021-03-19 11:22:02 +0800

    first version for PRD

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
 import AfterLogin from '../views/MainPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
   {
     path: '/',
     name: 'Main',
